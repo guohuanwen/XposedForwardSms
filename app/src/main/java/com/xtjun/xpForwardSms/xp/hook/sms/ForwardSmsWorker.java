ba63010e799acaad2a5a71cb5aced6606f8fada1
bigwen <1179636441@qq.com>
2024-07-20 20:46:14 +0800

    支持小愛通話轉發

--- app/src/main/java/com/xtjun/xpForwardSms/xp/hook/sms/ForwardSmsWorker.java
+++ app/src/main/java/com/xtjun/xpForwardSms/xp/hook/sms/ForwardSmsWorker.java
@@ -1,94 +1,1 @@
-package com.xtjun.xpForwardSms.xp.hook.sms;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.util.Log;
-
-import com.github.xtjun.xposed.forwardSms.BuildConfig;
-import com.xtjun.xpForwardSms.common.action.entity.SmsMsg;
-import com.xtjun.xpForwardSms.common.constant.MPrefConst;
-import com.xtjun.xpForwardSms.common.msp.MultiProcessSharedPreferences;
-import com.xtjun.xpForwardSms.common.utils.StringUtils;
-import com.xtjun.xpForwardSms.common.utils.XLog;
-import com.xtjun.xpForwardSms.common.utils.XSPUtils;
-import com.xtjun.xpForwardSms.xp.hook.sms.action.impl.ForwardSmsAction;
-import com.xtjun.xpForwardSms.xp.hook.sms.action.impl.SmsGetAction;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class ForwardSmsWorker {
-
-    private final SharedPreferences sp;
-    private final Intent mSmsIntent;
-    private final ScheduledExecutorService mScheduledExecutor;
-
-    ForwardSmsWorker(Context appContext, Intent smsIntent) {
-        sp = MultiProcessSharedPreferences.getSharedPreferences(appContext, MPrefConst.SP_NAME, Context.MODE_PRIVATE);
-        mSmsIntent = smsIntent;
-        mScheduledExecutor = Executors.newSingleThreadScheduledExecutor();
-    }
-
-    public ParseResult parse() {
-        if (!XSPUtils.isEnabled(sp)) {
-            XLog.i("XposedForwardSms disabled, exiting");
-            return null;
-        }
-
-        boolean verboseLog = XSPUtils.isVerboseLogMode(sp);
-        if (verboseLog) {
-            XLog.setLogLevel(Log.VERBOSE);
-        } else {
-            XLog.setLogLevel(BuildConfig.LOG_LEVEL);
-        }
-
-        //获取短信
-        SmsGetAction smsGetAction = new SmsGetAction(null, sp);
-        smsGetAction.setSmsIntent(mSmsIntent);
-        ScheduledFuture<Bundle> smsParseFuture = mScheduledExecutor.schedule(smsGetAction, 0, TimeUnit.MILLISECONDS);
-
-        SmsMsg smsMsg;
-        try {
-            Bundle parseBundle = smsParseFuture.get();
-            if (parseBundle == null) {
-                return null;
-            }
-            smsMsg = parseBundle.getParcelable(SmsGetAction.SMS_MSG);
-        } catch (Exception e) {
-            XLog.e("Error occurs when get SmsGetAction call value", e);
-            return null;
-        }
-
-        //过滤短信内容
-        boolean filterFlag = true;
-
-        if(XSPUtils.getFilterEnable(sp)){
-            XLog.d("getFilterEnable: " + XSPUtils.getFilterEnable(sp));
-            String filterKeywords = XSPUtils.getFilterKeywords(sp);
-            XLog.d("getFilterEnable: " + XSPUtils.getFilterEnable(sp));
-            if (StringUtils.isNotEmpty(filterKeywords)){
-                Matcher matcher = Pattern.compile(filterKeywords).matcher(smsMsg.getBody());
-                if (!matcher.find()) {
-                    filterFlag = false;
-                }
-            }
-        }
-
-        //转发短信
-        if (filterFlag) new Thread(new ForwardSmsAction(smsMsg, sp)).start();
-
-        return buildParseResult();
-    }
-
-    private ParseResult buildParseResult() {
-        ParseResult parseResult = new ParseResult();
-        parseResult.setBlockSms(false);
-        return parseResult;
-    }
-}
+package com.xtjun.xpForwardSms.xp.hook.sms;import android.content.Context;import android.content.Intent;import android.content.SharedPreferences;import android.os.Bundle;import android.util.Log;import com.github.xtjun.xposed.forwardSms.BuildConfig;import com.xtjun.xpForwardSms.common.action.entity.SmsMsg;import com.xtjun.xpForwardSms.common.constant.MPrefConst;import com.xtjun.xpForwardSms.common.msp.MultiProcessSharedPreferences;import com.xtjun.xpForwardSms.common.utils.StringUtils;import com.xtjun.xpForwardSms.common.utils.XLog;import com.xtjun.xpForwardSms.common.utils.XSPUtils;import com.xtjun.xpForwardSms.ui.app.ForwardSmsApplication;import com.xtjun.xpForwardSms.xp.hook.sms.action.impl.ForwardSmsAction;import com.xtjun.xpForwardSms.xp.hook.sms.action.impl.SmsGetAction;import java.util.concurrent.Executors;import java.util.concurrent.ScheduledExecutorService;import java.util.concurrent.ScheduledFuture;import java.util.concurrent.TimeUnit;import java.util.regex.Matcher;import java.util.regex.Pattern;public class ForwardSmsWorker {    private final SharedPreferences sp;    private final Intent mSmsIntent;    private final ScheduledExecutorService mScheduledExecutor;    ForwardSmsWorker(Context appContext, Intent smsIntent) {        sp = MultiProcessSharedPreferences.getSharedPreferences(appContext, MPrefConst.SP_NAME, Context.MODE_PRIVATE);        mSmsIntent = smsIntent;        mScheduledExecutor = Executors.newSingleThreadScheduledExecutor();    }    public ParseResult parse() {        if (!XSPUtils.isEnabled(sp)) {            XLog.i("XposedForwardSms disabled, exiting");            return null;        }        boolean verboseLog = XSPUtils.isVerboseLogMode(sp);        if (verboseLog) {            XLog.setLogLevel(Log.VERBOSE);        } else {            XLog.setLogLevel(BuildConfig.LOG_LEVEL);        }        //获取短信        SmsGetAction smsGetAction = new SmsGetAction(null, sp);        smsGetAction.setSmsIntent(mSmsIntent);        ScheduledFuture<Bundle> smsParseFuture = mScheduledExecutor.schedule(smsGetAction, 0, TimeUnit.MILLISECONDS);        SmsMsg smsMsg;        try {            Bundle parseBundle = smsParseFuture.get();            if (parseBundle == null) {                return null;            }            smsMsg = parseBundle.getParcelable(SmsGetAction.SMS_MSG);        } catch (Exception e) {            XLog.e("Error occurs when get SmsGetAction call value", e);            return null;        }        //过滤短信内容        boolean filterFlag = true;        if(XSPUtils.getFilterEnable(sp)){            XLog.d("getFilterEnable: " + XSPUtils.getFilterEnable(sp));            String filterKeywords = XSPUtils.getFilterKeywords(sp);            XLog.d("getFilterEnable: " + XSPUtils.getFilterEnable(sp));            if (StringUtils.isNotEmpty(filterKeywords)){                Matcher matcher = Pattern.compile(filterKeywords).matcher(smsMsg.getBody());                if (!matcher.find()) {                    filterFlag = false;                }            }        }        //转发短信        if (filterFlag) new Thread(new ForwardSmsAction(smsMsg, sp)).start();        return buildParseResult();    }    private ParseResult buildParseResult() {        ParseResult parseResult = new ParseResult();        parseResult.setBlockSms(false);        return parseResult;    }}
--- app/src/main/java/com/xtjun/xpForwardSms/xp/hook/sms/action/impl/ForwardSmsAction.java
+++ app/src/main/java/com/xtjun/xpForwardSms/xp/hook/sms/action/impl/ForwardSmsAction.java
@@ -1,73 +1,1 @@
-package com.xtjun.xpForwardSms.xp.hook.sms.action.impl;
-
-import android.content.SharedPreferences;
-import android.os.Bundle;
-
-import com.xtjun.xpForwardSms.common.action.RunnableAction;
-import com.xtjun.xpForwardSms.common.action.entity.SmsMsg;
-import com.xtjun.xpForwardSms.common.constant.Const;
-import com.xtjun.xpForwardSms.common.utils.StringUtils;
-import com.xtjun.xpForwardSms.common.utils.XHttpUtils;
-import com.xtjun.xpForwardSms.common.utils.XLog;
-import com.xtjun.xpForwardSms.common.utils.XSPUtils;
-
-/**
- * 记录验证码短信
- */
-public class ForwardSmsAction extends RunnableAction {
-
-    public ForwardSmsAction(SmsMsg smsMsg, SharedPreferences sp) {
-        super(smsMsg, sp);
-    }
-
-    @Override
-    public Bundle action() {
-        forwardSmsMsg(mSmsMsg);
-        return null;
-    }
-
-    private void forwardSmsMsg(SmsMsg smsMsg) {
-        String channelType = XSPUtils.getForwardChannelType(sp);
-        XLog.d("start forward: " + channelType);
-        String title = "卡"+smsMsg.getSubId()+"收到" + smsMsg.getSender() + "的新消息";
-        String content = smsMsg.getBody() + "\n--来自设备：【" + XSPUtils.getDeviceId(sp) + "】";
-        try {
-            boolean suc = false;
-            switch (channelType) {
-                case Const.CHANNEL_GET:
-                    suc = XHttpUtils.custGet(XSPUtils.getGetUrl(sp), title, content);
-                    break;
-                case Const.CHANNEL_POST:
-                    suc = XHttpUtils.custPost(XSPUtils.getPostUrl(sp), XSPUtils.getPostType(sp), XSPUtils.getPostContent(sp), title, content);
-                    break;
-                case Const.CHANNEL_DING:
-                    suc = XHttpUtils.postDingTalk(XSPUtils.getDingKey(sp), title, content);
-                    break;
-                case Const.CHANNEL_BARK:
-                    suc = XHttpUtils.getBark(XSPUtils.getBarkUrl(sp), title, content);
-                    break;
-                case Const.CHANNEL_WXCP:
-                    long now = System.currentTimeMillis();
-                    long expDate = sp.getLong("wxcp_expDate", 0L);
-                    String token = sp.getString("excp_token", "");
-                    if (now > (expDate + 3600000)){
-                        String wxcpToken = XHttpUtils.getWxcpToken(XSPUtils.getWxCorpid(sp), XSPUtils.getWxCorpsecret(sp));
-                        if (StringUtils.isNotEmpty(wxcpToken)) {
-                            token = wxcpToken;
-                            sp.edit().putLong("wxcp_expDate", now).putString("excp_token", wxcpToken).apply();
-                        }
-                    }
-                    suc = XHttpUtils.postWxcpMsg(token,XSPUtils.getWxAgentid(sp),XSPUtils.getWxTouser(sp),title,content);
-                    break;
-                case Const.CHANNEL_FEISHU:
-                    suc = XHttpUtils.postFeishuTalk(XSPUtils.getFeishuKey(sp), title, content);
-                    break;
-                default:
-                    break;
-            }
-            XLog.d("forward result: " + suc);
-        } catch (Exception e) {
-            XLog.e("forward error: " + e.getMessage());
-        }
-    }
-}
+package com.xtjun.xpForwardSms.xp.hook.sms.action.impl;import android.content.SharedPreferences;import android.os.Bundle;import com.xtjun.xpForwardSms.common.action.RunnableAction;import com.xtjun.xpForwardSms.common.action.entity.SmsMsg;import com.xtjun.xpForwardSms.common.constant.Const;import com.xtjun.xpForwardSms.common.utils.StringUtils;import com.xtjun.xpForwardSms.common.utils.XHttpUtils;import com.xtjun.xpForwardSms.common.utils.XLog;import com.xtjun.xpForwardSms.common.utils.XSPUtils;/** * 记录验证码短信 */public class ForwardSmsAction extends RunnableAction {    private SharedPreferences sp;    public ForwardSmsAction(SmsMsg smsMsg, SharedPreferences sp) {        super(smsMsg, sp);        this.sp = sp;    }    @Override    public Bundle action() {        forwardSmsMsg(mSmsMsg, this.sp);        return null;    }    public static void forwardSmsMsg(SmsMsg smsMsg, SharedPreferences sp) {        String channelType = XSPUtils.getForwardChannelType(sp);        XLog.d("start forward: " + channelType);        String title = "卡"+smsMsg.getSubId()+"收到" + smsMsg.getSender() + "的新消息";        String content = smsMsg.getBody() + "\n--来自设备：【" + XSPUtils.getDeviceId(sp) + "】";        try {            boolean suc = false;            switch (channelType) {                case Const.CHANNEL_GET:                    suc = XHttpUtils.custGet(XSPUtils.getGetUrl(sp), title, content);                    break;                case Const.CHANNEL_POST:                    suc = XHttpUtils.custPost(XSPUtils.getPostUrl(sp), XSPUtils.getPostType(sp), XSPUtils.getPostContent(sp), title, content);                    break;                case Const.CHANNEL_DING:                    suc = XHttpUtils.postDingTalk(XSPUtils.getDingKey(sp), title, content);                    break;                case Const.CHANNEL_BARK:                    suc = XHttpUtils.getBark(XSPUtils.getBarkUrl(sp), title, content);                    break;                case Const.CHANNEL_WXCP:                    long now = System.currentTimeMillis();                    long expDate = sp.getLong("wxcp_expDate", 0L);                    String token = sp.getString("excp_token", "");                    if (now > (expDate + 3600000)){                        String wxcpToken = XHttpUtils.getWxcpToken(XSPUtils.getWxCorpid(sp), XSPUtils.getWxCorpsecret(sp));                        if (StringUtils.isNotEmpty(wxcpToken)) {                            token = wxcpToken;                            sp.edit().putLong("wxcp_expDate", now).putString("excp_token", wxcpToken).apply();                        }                    }                    suc = XHttpUtils.postWxcpMsg(token,XSPUtils.getWxAgentid(sp),XSPUtils.getWxTouser(sp),title,content);                    break;                case Const.CHANNEL_FEISHU:                    suc = XHttpUtils.postFeishuTalk(XSPUtils.getFeishuKey(sp), title, content);                    break;                default:                    break;            }            XLog.d("forward result: " + suc);        } catch (Exception e) {            XLog.e("forward error: " + e.getMessage());        }    }}
